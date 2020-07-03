db6c6a0eaac5a4a987bae7a3b90b6a99ec2b702f
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-07-03 23:14:09 +0100

    Updates

---
+++ precache-manifest.d4fb89ab1b463c68f6621a415cd76734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fccab7e24af9f4b4b6620d77b34bff",
+    "url": "/tech-test/index.html"
+  },
+  {
+    "revision": "b31ec12085e963b5b139",
+    "url": "/tech-test/static/css/main.522bbe93.chunk.css"
+  },
+  {
+    "revision": "f401b2dbaa72a8b3b29a",
+    "url": "/tech-test/static/js/2.ea3fad5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tech-test/static/js/2.ea3fad5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31ec12085e963b5b139",
+    "url": "/tech-test/static/js/main.275c41aa.chunk.js"
+  },
+  {
+    "revision": "e53cdf7258c9f40463c1",
+    "url": "/tech-test/static/js/runtime-main.a73da4ab.js"
+  }
+]);
